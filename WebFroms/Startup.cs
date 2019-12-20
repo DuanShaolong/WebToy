bfb18bfc058076ccd8d96c0629557a1957c17256
段少龙 <DTiger@LIVE.CN>
2019-12-20 15:45:20 +0800

    Add project files.

---
+++ WebFroms/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFroms.Startup))]
+namespace WebFroms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
