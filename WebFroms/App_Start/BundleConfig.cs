1ed0266e0450b6ece39705b562bfda4dae9228cd
段少龙 <DTiger@LIVE.CN>
2019-12-21 00:19:10 +0800

    update bootstrap version to 4.4.1, update nuget package to lastest version.

--- WebFroms/App_Start/BundleConfig.cs
+++ WebFroms/App_Start/BundleConfig.cs
@@ -32,7 +32,10 @@ namespace WebFroms
             // Use the Development version of Modernizr to develop with and learn from. Then, when you’re
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                            "~/Scripts/modernizr-*"));
+                            "~/Scripts/modernizr-*",
+                            "~/Scripts/jquery-*",
+                            "~/Scripts/bootstrap.js",
+                            "~/Scripts/popper.js"));
         }
     }
 }
